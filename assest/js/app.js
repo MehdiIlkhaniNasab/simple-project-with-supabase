074670e22df5fe9099d5cb9df642f0479b8f98ce
Mehdi Ilkhani Nasab <mehdiilkhaninasab7@gmail.com>
2023-02-05 10:14:48 +0330

    revision project

--- assest/js/app.js
+++ assest/js/app.js
@@ -3,7 +3,6 @@ const loginBtnTitle = document.getElementById('login');
 const signupBtnTitle = document.getElementById('signup');
 const signupBtn = document.querySelector('.signup .submit-btn')
 const loginBtn = document.querySelector('.login .submit-btn')
-const modalContainer = document.querySelector('.modal')
 
 
 loginBtnTitle.addEventListener('click', (e) => {
@@ -30,135 +29,8 @@ signupBtnTitle.addEventListener('click', (e) => {
     });
 });
 
-
-// Validate Inputs
-const validateInput = (input, regex, flag = 'g') => {
-    const regexValidate = new RegExp(regex, flag)
-    const inputValue = input.value.trim()
-    const resultValidate = regexValidate.test(inputValue)
-    return resultValidate;
+function signupNewUser() {
+    
 }
 
-
-
-function showModalContent(container, condition, titleModal) {
-    container.classList.add('active')
-
-    container.innerHTML = ''
-    if (condition) {
-        container.insertAdjacentHTML('beforeend',
-            `<div class="modal-wrapper active modal-success">
-            <h3 class="modal-title ">Sucess</h3>
-            <p class="modal-description">Your ${titleModal} Is Done Successfuly</p>
-            <button class="modal-btn btn-success" >Ok</button>
-        </div>`)
-    } else {
-        container.insertAdjacentHTML('beforeend',
-            `<div class="modal-wrapper active modal-danger">
-            <h3 class="modal-title ">Unsucess</h3>
-            <p class="modal-description">Your ${titleModal} Is Failed</p>
-            <button class="modal-btn btn-danger" >Ok</button>
-        </div>`)
-    }
-    let modalBtn = document.querySelector('.modal-btn')
-    modalBtn.addEventListener('click', closeModal)
-}
-
-function closeModal() {
-    const modalContainer = document.querySelector('.modal')
-    const modalWrapper = document.querySelector('.modal-wrapper')
-    modalContainer.classList.remove('active')
-    modalWrapper.classList.remove('active')
-}
-
-// Fetch Insert New Row
-async function insertNewRow(tableName, newInfoObj) {
-    const { error } = await supabaseCon
-        .from(tableName)
-        .insert([newInfoObj])
-    if (error) {
-        return false
-    } else {
-        return true
-    }
-
-}
-
-// Fetch Get User filter by email
-async function getInfoUser(tableName, emailUser) {
-    const { data, error } = await supabaseCon
-        .from(tableName)
-        .select()
-        .eq('email', emailUser)
-    if (error) {
-        return false
-    }
-    if (data) {
-        return data[0]
-    }
-}
-
-
-loginBtn.addEventListener('click', async () => {
-
-
-    const emailInput = document.querySelector('.login .email-input')
-    const passwordInput = document.querySelector('.login .password-input')
-    const modalContainer = document.querySelector('.modal')
-    modalContainer.classList.add('active')
- 
-   const targetUser = await  getInfoUser('users', emailInput.value.trim())
-   
-    if (targetUser.password == passwordInput.value.trim()) {
-        showModalContent(modalContainer, true, 'Login')
-    } else {
-        showModalContent(modalContainer, false, 'Login')
-    }
-
-
-})
-
-signupBtn.addEventListener('click', () => {
-
-
-    const infoNewUser = getInfoNewUser()
-    const resultFetch = insertNewRow('users', infoNewUser)
-
-    if (infoNewUser && resultFetch) {
-        showModalContent(modalContainer, true, 'Register')
-    } else {
-        showModalContent(modalContainer, false, 'Register')
-    }
-})
-
-
-function getInfoNewUser() {
-
-    const regexValidateName = /^[\w]{3,12}/
-    const regexValidateEmail = /^([\w]+.?\w+)+@([a-z]{3,5}).([a-z]{2,3})/
-    const regexValidatePass = /[\w{3,12}]/
-
-    const nameInput = document.querySelector('.signup .name-input')
-    const emailInput = document.querySelector('.signup .email-input')
-    const passwordInput = document.querySelector('.signup .password-input')
-
-    const validateName = validateInput(nameInput, regexValidateName)
-    const validateEmail = validateInput(emailInput, regexValidateEmail)
-    const validatePass = validateInput(passwordInput, regexValidatePass)
-
-    const resultValidate = validateEmail && validatePass && validateName
-    if (resultValidate) {
-        let newUser = {
-            name: nameInput.value,
-            email: emailInput.value,
-            password: passwordInput.value
-        }
-        return newUser
-    } else {
-        return false
-    }
-}
-
-
-
-
+signupBtn.addEventListener('click', signupNewUser)
